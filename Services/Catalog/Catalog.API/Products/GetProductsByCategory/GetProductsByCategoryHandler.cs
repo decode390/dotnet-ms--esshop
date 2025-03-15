7063422a7999704f37b8116c661ea56cfcd93a59
decode390 <decode390@gmail.com>
2025-03-15 18:13:11 -0500

    (category.api) get products by category)

---
+++ Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,20 @@
+namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductsByCategoryQuery(string Category): IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductsByCategoryQueryHandler(
+    ILogger<GetProductsByCategoryQueryHandler> logger,
+    IDocumentSession session
+)
+: IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+        var products = await session.Query<Product>().Where(p => p.Category.Any(c => c == query.Category)).ToListAsync(cancellationToken);
+
+        // var product = await session.Query<Product>().Where(o => o.Id == query.Id).FirstOrDefaultAsync(cancellationToken);
+        return new GetProductsByCategoryResult(products);
+    }
+}
